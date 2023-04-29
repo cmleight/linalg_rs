0ada3a9e01de41b9563f1a03135018146731eea6
Chris Leight <chris@leightc.net>
2023-04-29 15:32:59 -0400

    add broken laplace_determinant function, revisit it after lu decomp

--- src/main.rs
+++ src/main.rs
@@ -61,11 +61,35 @@ struct Matrix<T: IntFloat> {
     data: Vec<T>,
 }
 
+fn modulo_euclidean(a: i64, b: i64) -> usize {
+    let m = a % b;
+    let res = if m < 0 {
+        if b < 0 {
+            m - b
+        } else {
+            m + b
+        }
+    } else {m};
+    return res as usize
+}
+
 impl<T: IntFloat> Matrix<T> {
-    pub fn determinant(&self) {
-        (0..self.rows).map(|i| {
-            
+    pub fn laplace_determinant(&self) -> T {
+        return (0..self.rows).map(|i| {
+            (0..self.cols).map(|j| {
+                (self.data[modulo_euclidean((i + j) as i64, self.rows as i64) + j * self.cols], if i + 1 < self.rows {
+                    self.data[modulo_euclidean(i as i64 + 1 - j as i64, self.rows as i64) + j * self.cols]
+                } else {
+                    T::zero()
+                })
+            }).reduce(|(add_0, sub_0), (add_1, sub_1)| (add_0  * add_1, sub_0 * sub_1))
         })
+        .map(|term| match term {
+            Some((add, sub)) => add - sub,
+            None => T::zero(),
+        })
+        .reduce(|a, b| a + b)
+        .unwrap()
     }
 }
 
@@ -73,9 +97,10 @@ fn main() {
     let a = Matrix {
         rows: 3,
         cols: 3,
-        data: vec![1., 2., 3.,
-                   4., 5., 6.,
-                   7., 8., 9.],
+        data: vec![1., 2., 2.,
+                   2., 0., -1.,
+                   -2., 1., 3.],
     };
+    println!("determinant: {}", a.laplace_determinant())
 
 }
